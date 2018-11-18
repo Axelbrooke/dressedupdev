d94d33d9896d9a8b57aafd5f7ce051edf1d1544c
Carina Carter <CarinaBrooke@gmail.com>
2018-11-18 22:41:03 +0900

    First commit

---
+++ src/main.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <div><i className="material-icons">menu</i></div>
+                <h1 className="title flex center">Dressed Up Dev</h1>
+                <main className="main-padding-side flex center ">
+                    <div className="column content-padding separator-line content-font content-size">
+                        <p>To me Japan has been, and will always be, magic. However, I do not think that this sentiment
+                            is unique to only me. Japan has captured hearts all around the world ever since Nintendo and
+                            anime found its way over the ocean. So for all you yearning to have a go at living and
+                            working in one of the most magical places in the world, I have decided to write a guide
+                            about finding a development job in Japan. Let’s go!</p>
+
+                        <p>Decaffeinated cinnamon cortado, french press sit decaffeinated a macchiato skinny. Americano
+                            body to go
+                            aftertaste grinder macchiato kopi-luwak. Barista percolator, qui strong con panna froth
+                            single origin. Extra
+                            spoon sit mug beans macchiato grounds. Shop cappuccino, bar beans brewed cultivar that.</p>
+
+                        <p>Whipped froth single shot, flavour frappuccino, beans, cup, wings medium qui espresso
+                            caffeine. Dripper,
+                            arabica
+                            cultivar french press kopi-luwak, aromatic ut, body robust affogato pumpkin spice aged.
+                            Acerbic seasonal,
+                            single
+                            origin crema ut frappuccino extra shop. And white foam siphon single origin crema
+                            extraction. Redeye ut
+                            dripper,
+                            caramelization a body mazagran doppio skinny cortado.</p>
+
+                        <p>Grinder brewed barista cup cream arabica strong. Breve id, sweet, saucer extra black ut
+                            robust skinny. Beans,
+                            extraction black dark grinder beans decaffeinated est decaffeinated. Body, instant aroma
+                            roast so, extra
+                            breve,
+                            whipped macchiato decaffeinated con panna to go. At est strong caffeine instant est
+                            kopi-luwak.</p>
+                    </div>
+                    <div className="column content-padding j-font">
+                        <p>心の中に夢があって、２年前日本に引っ越してきました。すごく素敵な冒険になることと気づかなかったんです。</p>
+
+                        <p>Decaffeinated cinnamon cortado, french press sit decaffeinated a macchiato skinny. Americano
+                            body to go
+                            aftertaste grinder macchiato kopi-luwak. Barista percolator, qui strong con panna froth
+                            single origin. Extra
+                            spoon sit mug beans macchiato grounds. Shop cappuccino, bar beans brewed cultivar that.</p>
+
+                        <p>Whipped froth single shot, flavour frappuccino, beans, cup, wings medium qui espresso
+                            caffeine. Dripper,
+                            arabica
+                            cultivar french press kopi-luwak, aromatic ut, body robust affogato pumpkin spice aged.
+                            Acerbic seasonal,
+                            single
+                            origin crema ut frappuccino extra shop. And white foam siphon single origin crema
+                            extraction. Redeye ut
+                            dripper,
+                            caramelization a body mazagran doppio skinny cortado.</p>
+
+                        <p>Grinder brewed barista cup cream arabica strong. Breve id, sweet, saucer extra black ut
+                            robust skinny. Beans,
+                            extraction black dark grinder beans decaffeinated est decaffeinated. Body, instant aroma
+                            roast so, extra
+                            breve,
+                            whipped macchiato decaffeinated con panna to go. At est strong caffeine instant est
+                            kopi-luwak.</p>
+                    </div>
+                </main>
+            </div>
+        );
+    }
+}
+
+export default Main;
